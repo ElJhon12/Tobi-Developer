e6f6ec8e570448d9e854a3d237dd8cbcfc79c570
invalid name <jhonpsn12@outlook.com>
2021-03-04 18:29:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+app.get('/',function (req, res) {
+  res.send('Tobi is Online!')
+});
+
+ let port = process.env.PORT || 3000;
+ app.listen(port)
+  
+ require('dotenv').config()
+ ///////////////////////////////////////////////////
+
+const { Discord, Client } = require("discord.js");
+
+const client = new Client;
+
+client.on('ready', () => {
+
+console.log('Tobi Is Online');
+
+});
+
+const OWNER = "817019120570466354";
+
+//CONFIG
+var prefix = process.env.PREFIX || "t.";
+
+var token = process.env.TOKEN;
+
+client.login(token);
+
+//END CONFIGS
+
+client.on('message', async (msg) => {
+if (!msg.content.startsWith(prefix)) return;
+if (msg.author.bot) return;
+
+if (msg.channel.type === "dm") {
+return;
+}
+
+if (!msg.guild) return;
+
+const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+if(command === 'test'){
+  msg.reply('Working!');
+}
+
+});
